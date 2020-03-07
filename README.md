1e8719989bf6fc461e168c4637adbba081e560b9
Tcom242242 <tcom242242@gmail.com>
2020-03-07 16:28:05 +0900

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+### simulator.py
+実験実行用のスクリプト
+
+基本的にはgridとagentの管理及び、画面の描画を行う
+
+
+### grid_env.py
+grid__world
+
+### agents
+エージェントが格納されている
--- simulator.py
+++ simulator.py
@@ -50,36 +50,6 @@ def reset_grid():
     return grid
 
 
-def is_in_grid(x, y):
-    """
-        x, yがグリッドワールド内かの確認
-    """
-    if y < len(grid) and y >= 0:
-        if x < len(grid[0]) and x >= 0:
-            return True
-    return False
-
-
-def update_agent_pos(x, y):
-    """
-        エージェントの位置の更新 
-    """
-
-    while True:
-        to_y, to_x = y, x
-        action = np.random.randint(6)
-        if action == ACTIONS["UP"]:
-            to_y += -1
-        elif action == ACTIONS["DOWN"]:
-            to_y += 1
-        elif action == ACTIONS["LEFT"]:
-            to_x += -1
-        elif action == ACTIONS["RIGHT"]:
-            to_x += 1
-
-        if is_in_grid(to_y, to_x) is True:
-            return to_x, to_y
-
 
 def draw_grid_world(grid):
     """
@@ -157,29 +127,5 @@ if __name__ == '__main__':
         agent.observe(state)    # エージェントを初期位置に
         is_end_episode = False
 
-    # while not done:
-    #     screen.fill(BLACK)
-    #
-    #     # grid worldの描画
-    #     draw_grid_world()
-    #
-    #     # テキストを描画したSurfaceを作成
-    #     step_str = sysfont.render("step:{}".format(step), False, WHITE)
-    #     
-    #     # 位# テキストを描画する
-    #     screen.blit(step_str, (500,50))
-    #
-    #     clock.tick(1)
-    #     step += 1
-    #
-    #     # 再描画
-    #     pygame.display.flip()
-    #
-    #     # エージェントの位置の更新
-    #     to_x, to_y = update_agent_pos(x, y)
-    #
-    #     grid[y][x] = 0
-    #     grid[to_y][to_x] = 1
-    #     x, y = to_x, to_y
 
     pygame.quit()
